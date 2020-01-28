c85a72e9b023a75fcc6fcd11dab39430b6057b73
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2020-01-28 07:55:56 +0000

    Done.

--- app.rb
+++ app.rb
@@ -20,6 +20,8 @@ class App < Sinatra::Base
 
   get '/set' do
     # set the :foo key of the session hash equal to 'hello' here!
+    session[:foo] = "hello"
+    
     if session[:foo] == 'hello'
       redirect '/fetch'
     else
@@ -33,10 +35,11 @@ class App < Sinatra::Base
 
   get '/second_exercise' do
     "Your second lesson will be to set your session :id key to a value.\nIn the route: /set_session, write a line of code that sets the session[:id] equal to 1.\nThen, navigate to the '/set_session' path"
+    
   end
 
   get '/set_session' do
-    #set session id here
+    session[:id] = 1 
 
     if session[:id] == 1
       # "Session ID set. It's currently set to #{session[:id]}."
@@ -51,7 +54,8 @@ class App < Sinatra::Base
   end
 
   get '/logout' do
-    #clear session hash here
+    #clear session
+    session[:id].clear
     "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
   end
 
